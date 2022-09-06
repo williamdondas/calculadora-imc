42981be3f64776ef457c0f9de264fc6288b590c9
William Dondas <williamdondas@hotmail.com>
2022-09-06 19:51:36 -0300

    Update calcImc.js

--- calcImc.js
+++ calcImc.js
@@ -9,16 +9,23 @@ botao.addEventListener("click",function(event) {
     var peso = document.getElementById("peso").value;
     var altura = document.getElementById("altura").value;
 
+    var classificacao = "";
+
     if(peso=="" || altura=="" || nome==""){
         resultado.textContent="um ou mais campos não foram preenchidos corretamente, tente novamente";
     }else{
         var imc = peso/(altura*altura);
 
-    resultado.textContent = `Olá ${nome}, o resultado do seu imc é: ${imc.toFixed(2)} `;
+        if(imc <18.5){
+            classificacao = "voce está abaixo do seu peso ideal!."
+        }else{
+            classificacao = "você está dentro do seu peso ideal!"
+        }
+
+        resultado.textContent = `Olá ${nome}, o resultado do seu imc é: ${imc.toFixed(2)} , ${classificacao}`;
     }
 
-     
+    
+    
 
 })
-
-
